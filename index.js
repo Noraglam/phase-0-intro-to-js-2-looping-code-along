7cddafedac418a6404cd5cf11349b4fde030d4a8
Nora <noraonyi@gmail.com>
2022-03-27 23:48:14 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function writecard (arr,event) {
+    
+for(let i=0; i<3; 1 +1){
+ //Runs 5 times with values of steo 0 through 4
+}
+Return [//= = = 
+"thank you"
